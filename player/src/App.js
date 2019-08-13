6095c88adf1f61f26d0a7be6f77d6adbc890d712
brettmadrid <brettmadrid9@gmail.com>
2019-08-12 23:30:12 -0700

    usecontext setup example

--- player/src/App.js
+++ player/src/App.js
@@ -1,25 +1,35 @@
-import React, { useState, createContext } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 import './App.css';
+
+// STEP 1: create a context object
+const NameContext = createContext()
+console.log(NameContext.Provider)
+
 
 function App() {
   const [name, setName] = useState('Harry Potter')
   return (
+    // STEP 2: wrap component tree inside Provider component
+    // STEP 3: pass state data to the 'value' prop of the Provider and remove passing of prop directly to the child
+  <NameContext.Provider value={name}>
     <div className="App">
-      <Child name={name}/>
+      {/* <Child name={name}/> */}
+      <Child />
     </div>
+  </NameContext.Provider>
   );
 }
 
 export default App;
 
-const Child = (props) => {
+// STEP 4: consume the data in some child component
+const Child = props => {
   return (
-    <GrandChild name={props.name} />
+    <GrandChild />
   )
 }
 
-const GrandChild = props => {
-  return (
-    <h2>{props.name}</h2>
-  )
+const GrandChild = () => {
+    const name = useContext(NameContext)
+    return <h2>{name}</h2>
 }
