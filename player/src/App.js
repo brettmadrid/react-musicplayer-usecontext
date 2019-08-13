696d943fa87be4d8ab7d002a43f8786c90e57629
brettmadrid <brettmadrid9@gmail.com>
2019-08-12 22:58:34 -0700

    Setup App with prop drilling first

--- player/src/App.js
+++ player/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, createContext } from 'react';
 import './App.css';
 
 function App() {
+  const [name, setName] = useState('Harry Potter')
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Child name={name}/>
     </div>
   );
 }
 
 export default App;
+
+const Child = (props) => {
+  return (
+    <GrandChild name={props.name} />
+  )
+}
+
+const GrandChild = props => {
+  return (
+    <h2>{props.name}</h2>
+  )
+}
